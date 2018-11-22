6433099c627e15911ee140953fe44f583b782512
David Whaley <davidwhaleycode@outlook.com>
2018-11-22 17:00:25 -0600

    init

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie); //view "helper" method inherited from the base controller class
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pagendex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
