3682f4d6e750efaacebd9d2c025aa2551340e75c
David Whaley <davidwhaleycode@outlook.com>
2018-11-23 10:07:54 -0600

    'creategigmodel'

---
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; } //add DbSet type Gig so entity framework know about the gigs class
+        public DbSet<Genre> Genres { get; set; } //add so list of genres can be queryed.
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
