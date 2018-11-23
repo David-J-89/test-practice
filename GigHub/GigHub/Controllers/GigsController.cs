68c211c46d0b6be910a0c79eeadebff5d1f9a8c0
David Whaley <davidwhaleycode@outlook.com>
2018-11-23 11:21:36 -0600

    'viewModels'

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController() //initialize _context with a constructor
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList() //allowing the list of genres to be pulled from the database, to allow for the dropdownlist to work
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; } //to populate dropdownlist from db, IEnumerable is the simplest interface that we can use for what we want.
+    }
+}
