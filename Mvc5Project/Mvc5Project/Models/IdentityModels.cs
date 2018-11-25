abaffad9124be9366752a9eb63c81260cad57a27
David Whaley <davidwhaleycode@outlook.com>
2018-11-24 22:00:12 -0600

    'BlogDbContext'

--- Mvc5Project/Mvc5Project/Models/IdentityModels.cs
+++ Mvc5Project/Mvc5Project/Models/IdentityModels.cs
@@ -1,8 +1,9 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Mvc5Project.Models
 {
@@ -28,6 +29,47 @@ namespace Mvc5Project.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+    }
+
+    public class BlogDbContext : DbContext
+    {
+
+        public BlogDbContext() : base("BlogConnection")
+        {
+
+        }
+
+        public static BlogDbContext Create()
+        {
+            return new BlogDbContext();
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Reply> Replies { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<PostTag> PostTags { get; set; }
+
+        public DbSet<PostCategory> PostCategories { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<PostVideo> PostVideos { get; set; }
+
+        public DbSet<PostLike> PostLikes { get; set; }
+
+        public DbSet<CommentLike> CommentLikes { get; set; }
+
+        public DbSet<ReplyLike> ReplyLikes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
     }
 }
