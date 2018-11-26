d1fc2ad9ceba5a85bdeb3d0648c13bd133e6e548
David Whaley <davidwhaleycode@outlook.com>
2018-11-25 18:33:04 -0600

    'finishseed'

--- Mvc5Project/Mvc5Project/Migrations/BlogDbContext/Configuration.cs
+++ Mvc5Project/Mvc5Project/Migrations/BlogDbContext/Configuration.cs
@@ -1,24 +1,150 @@
 namespace Mvc5Project.Migrations.BlogDbContext
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Mvc5Project.Models.BlogDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             MigrationsDirectory = @"Migrations\BlogDbContext";
         }
 
         protected override void Seed(Mvc5Project.Models.BlogDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Categories.AddOrUpdate(new Models.Category { Id = "cat1", Name = "Lorem", UrlSeo = "Lorem", Description = "Lorem Category" });
+            context.Categories.AddOrUpdate(new Models.Category { Id = "cat2", Name = "Duis", UrlSeo = "Duis", Description = "Duis Category" });
+            context.Categories.AddOrUpdate(new Models.Category { Id = "cat3", Name = "Nulla", UrlSeo = "Nulla", Description = "Nulla Category" });
+            context.Categories.AddOrUpdate(new Models.Category { Id = "cat4", Name = "Ipsum", UrlSeo = "Ipsum", Description = "Ipsum Category" });
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            context.Posts.AddOrUpdate(new Models.Post
+            {
+                Id = "1",
+                Title = "Lorem",
+                Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk" +
+                " djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget" +
+                " tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if" +
+                " dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at " +
+                "ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum" +
+                " dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac " +
+                "loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing " +
+                "elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk " +
+                "djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor" +
+                " pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd." +
+                "dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei" +
+                " commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut" +
+                " rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi " +
+                "at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd" +
+                " if dfdjfd fd.dsfds fdk.",
+                ShortDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis" +
+                " ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.",
+                PostedOn = DateTime.Now,
+                Meta = "Lorem",
+                UrlSeo = "Lorem",
+                Published = true
+            });
+
+            context.Posts.AddOrUpdate(new Models.Post
+            {
+                Id = "2",
+                Title = "Duis",
+                Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk" +
+                " djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget" +
+                " tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if" +
+                " dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at " +
+                "ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum" +
+                " dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac " +
+                "loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing " +
+                "elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk " +
+                "djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor" +
+                " pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd." +
+                "dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei" +
+                " commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut" +
+                " rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi " +
+                "at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd" +
+                " if dfdjfd fd.dsfds fdk.",
+                ShortDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis" +
+                " ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.",
+                PostedOn = DateTime.Now,
+                Meta = "Duis",
+                UrlSeo = "Duis",
+                Published = true
+            });
+
+            context.Posts.AddOrUpdate(new Models.Post
+            {
+                Id = "3",
+                Title = "Nulla",
+                Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk" +
+                " djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget" +
+                " tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if" +
+                " dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at " +
+                "ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum" +
+                " dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac " +
+                "loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing " +
+                "elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk " +
+                "djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor" +
+                " pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd." +
+                "dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei" +
+                " commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut" +
+                " rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi " +
+                "at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd" +
+                " if dfdjfd fd.dsfds fdk.",
+                ShortDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis" +
+                " ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.",
+                PostedOn = DateTime.Now,
+                Meta = "Nulla",
+                UrlSeo = "Nulla",
+                Published = true
+            });
+
+            context.Posts.AddOrUpdate(new Models.Post
+            {
+                Id = "4",
+                Title = "Ipsum",
+                Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk" +
+                " djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget" +
+                " tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if" +
+                " dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at " +
+                "ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum" +
+                " dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac " +
+                "loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing " +
+                "elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk " +
+                "djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor" +
+                " pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd." +
+                "dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei" +
+                " commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut" +
+                " rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi " +
+                "at risus eget tortor pharetra cursus eu at ligula. mausis ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd" +
+                " if dfdjfd fd.dsfds fdk.",
+                ShortDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus morbi at risus eget tortor pharetra cursus eu at ligula. mausis" +
+                " ei commod nisl,, ac loboaier lenckjdf, ut rhod kdjfd oewi djfdk djdkf . ksdjfdk. dkjdjfd if dfdjfd fd.dsfds fdk.",
+                PostedOn = DateTime.Now,
+                Meta = "Ipsum",
+                UrlSeo = "Upsum",
+                Published = true
+            });
+
+            context.PostCategories.AddOrUpdate(new Models.PostCategory { PostId = "1", CategoryId = "cat1" });
+            context.PostCategories.AddOrUpdate(new Models.PostCategory { PostId = "1", CategoryId = "cat4" });
+            context.PostCategories.AddOrUpdate(new Models.PostCategory { PostId = "2", CategoryId = "cat2" });
+            context.PostCategories.AddOrUpdate(new Models.PostCategory { PostId = "3", CategoryId = "cat3" });
+
+            context.PostVideos.AddOrUpdate(new Models.PostVideo { Id = 1, PostId = "1", VideoSiteName = "Youtube", VideoUrl = "https://www.youtube.com/embed/HcSEU_BZwDw", VideoThumbnail = "http://img.youtube.com/vi/HcSEU_BZwSw/0.jpg" });
+            context.PostVideos.AddOrUpdate(new Models.PostVideo { Id = 1, PostId = "2", VideoSiteName = "Youtube", VideoUrl = "https://www.youtube.com/embed/HcSEU_BZwDw", VideoThumbnail = "http://img.youtube.com/vi/HcSEU_BZwSw/0.jpg" });
+            context.PostVideos.AddOrUpdate(new Models.PostVideo { Id = 1, PostId = "3", VideoSiteName = "Youtube", VideoUrl = "https://www.youtube.com/embed/HcSEU_BZwDw", VideoThumbnail = "http://img.youtube.com/vi/HcSEU_BZwSw/0.jpg" });
+            context.PostVideos.AddOrUpdate(new Models.PostVideo { Id = 1, PostId = "1", VideoSiteName = "Youtube", VideoUrl = "https://www.youtube.com/embed/HcSEU_BZwDw", VideoThumbnail = "http://img.youtube.com/vi/HcSEU_BZwSw/0.jpg" });
+
+
         }
     }
 }
