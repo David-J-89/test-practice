c6f6cc612c6d7ab04750ecd9d050d80990aaed12
david-j-89 <davidwhaleycode@outlook.com>
2018-12-20 19:11:48 -0600

    'userclass

--- ChatTest/ChatTest/Models/ChatContext.cs
+++ ChatTest/ChatTest/Models/ChatContext.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace ChatTest.Models
 {
-    public class ChatContext
+    public class ChatContext: DbContext
     {
+        public ChatContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public static ChatContext Create()
+        {
+            return new ChatContext();
+        }
+
+        public DbSet<User> Users { get; set; }
     }
 }
