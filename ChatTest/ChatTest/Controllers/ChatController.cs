d1e46ddc1816c0eaf06161cff2e77e72295f0573
david-j-89 <davidwhaleycode@outlook.com>
2018-12-20 19:54:04 -0600

    'controllersrefactor'

---
+++ ChatTest/ChatTest/Controllers/ChatController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatTest.Controllers
+{
+    public class ChatController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["user"] == null)
+            {
+                return Redirect("/");
+            }
+
+            var currentUser = (Models.User)Session["user"];
+
+            using (var db = new Models.ChatContext())
+            {
+                ViewBag.allUsers = db.Users.Where(u => u.name != currentUser.name)
+                    .ToList();
+            }
+
+            ViewBag.currentUser = currentUser;
+
+            return View();
+        }
+    }
+}
