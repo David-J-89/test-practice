18bffdbf3023298ed4960b52283fb9abe0301384
david-j-89 <davidwhaleycode@outlook.com>
2018-12-20 20:00:33 -0600

    'conversationwithcontactmethod'

--- ChatTest/ChatTest/Controllers/ChatController.cs
+++ ChatTest/ChatTest/Controllers/ChatController.cs
@@ -27,5 +27,33 @@ namespace ChatTest.Controllers
 
             return View();
         }
+
+        public JsonResult ConversationWithContact(int contact)
+        {
+            if (Session["user"] == null)
+            {
+                return Json(new { status = "error", message = "User is not logged in" });
+            }
+
+            var currentUser = (Models.User)Session["user"];
+
+            var conversations = new List<Models.Conversation>();
+
+            using (var db = new Models.ChatContext())
+            {
+                conversations = db.Conversations.
+                    Where(c => (c.receiver_id == currentUser.id
+                    && c.sender_id == contact) ||
+                    (c.receiver_id == contact
+                    && c.sender_id == currentUser.id))
+                    .OrderBy(c => c.created_at)
+                    .ToList();
+            }
+
+            return Json(
+                new { status = "success", data = conversations },
+                JsonRequestBehavior.AllowGet
+                );
+        }
     }
 }
