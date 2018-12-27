2debb9805226016203c8d0a9df2e1493b10dba34
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:56:40 -0600

    Model for PostLike

--- RefactorBlog/RefactorBlog.Models/Models/PostLike.cs
+++ RefactorBlog/RefactorBlog.Models/Models/PostLike.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RefactorBlog.Models.Models
 {
     class PostLike
     {
+        [Key]
+        public string PostId { get; set; }
+
+        public string Username { get; set; }
+
+        public bool Like { get; set; }
+
+        public bool Dislike { get; set; }
+
+        public Post Post { get; set; }
     }
 }
