0e26dd847dc91cef0f5b711a989f6eac24833f77
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:53:54 -0600

    Model for Tag

--- RefactorBlog/RefactorBlog.Models/Models/Tag.cs
+++ RefactorBlog/RefactorBlog.Models/Models/Tag.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RefactorBlog.Models.Models
 {
     class Tag
     {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "UrlSeo")]
+        public string UrlSeo { get; set; }
+
+        public bool Checked { get; set; }
+
+        public ICollection<PostTag> PostTags { get; set; }
     }
 }
