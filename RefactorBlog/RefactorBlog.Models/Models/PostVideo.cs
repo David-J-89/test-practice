488f5ce0471bb50e14dee6618c76d854501b845b
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:55:41 -0600

    Model for PostVideo

--- RefactorBlog/RefactorBlog.Models/Models/PostVideo.cs
+++ RefactorBlog/RefactorBlog.Models/Models/PostVideo.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RefactorBlog.Models.Models
 {
     class PostVideo
     {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "VideoUrl")]
+        [DataType(DataType.Url)]
+        public string VideoUrl { get; set; }
+
+        public string VideoThumbnail { get; set; }
+
+        public string PostId { get; set; }
+
+        public string VideoSiteName { get; set; }
+
+        public Post Post { get; set; }
     }
 }
