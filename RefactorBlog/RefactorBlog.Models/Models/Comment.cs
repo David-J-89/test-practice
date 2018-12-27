45e8d650c2f3400066abd74d90f1997da6263d47
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:50:35 -0600

    Model for Comment

--- RefactorBlog/RefactorBlog.Models/Models/Comment.cs
+++ RefactorBlog/RefactorBlog.Models/Models/Comment.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RefactorBlog.Models.Models
 {
     class Comment
     {
+        public string Id { get; set; }
+
+        public string PostId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Username { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [DefaultValue(0)]
+        public int NetLikeCount { get; set; }
+
+        [DefaultValue(false)]
+        public bool Deleted { get; set; }
+
+        public Post Post { get; set; }
+
+        public ICollection<Reply> Replies { get; set; }
+
+        public ICollection<CommentLike> CommentLikes { get; set; }
     }
 }
