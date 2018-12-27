f7770fe24b2593f576cfcc2838fda8d6897dd125
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:46:10 -0600

    Model for Category

--- RefactorBlog/RefactorBlog.Models/Models/Category.cs
+++ RefactorBlog/RefactorBlog.Models/Models/Category.cs
@@ -1,6 +1,26 @@
-﻿namespace RefactorBlog.Models.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RefactorBlog.Models.Models
 {
     class Category
     {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "UrlSeo")]
+        public string UrlSeo { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        public bool Checked { get; set; }
+
+        public ICollection<PostCategory> PostCategories { get; set; }
     }
 }
