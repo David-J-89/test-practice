02fbc141b8118c045a9aee89975602bf2943debe
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:49:03 -0600

    Model for PostCategory

--- RefactorBlog/RefactorBlog.Models/Models/PostCategory.cs
+++ RefactorBlog/RefactorBlog.Models/Models/PostCategory.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RefactorBlog.Models.Models
 {
     class PostCategory
     {
+        [Key]
+        [Column(Order = 0)] //lookup what this does.
+        public string PostId { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public string CategoryId { get; set; }
+
+        public bool Checked { get; set; }
+
+        public Post Post { get; set; }
+
+        public Category Category { get; set; }
+
     }
 }
