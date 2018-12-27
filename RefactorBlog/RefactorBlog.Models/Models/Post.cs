c4cc541863ecad574f3e662af511b723d924eb6d
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:41:57 -0600

    Post Model

--- RefactorBlog/RefactorBlog.Models/Models/Post.cs
+++ RefactorBlog/RefactorBlog.Models/Models/Post.cs
@@ -1,10 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RefactorBlog.Models.Models
 {
     class Post
     {
+
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "ShortDescription")]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Body")]
+        public string Body { get; set; }
+
+        [Required]
+        [Display(Name = "Meta")]
+        public string Meta { get; set; }
+
+        [Required]
+        [Display(Name = "UrlSeo")]
+        public string UrlSeo { get; set; }
+
+        public bool Published { get; set; }
+
+        [DefaultValue(0)]
+        public int NetLikeCount { get; set; }
+
+        public DateTime PostedOn { get; set; }
+
+        public DateTime? Modified { get; set; }
+
+        //ICollections to associate with each post.
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<Reply> Replies { get; set; }
+        public ICollection<PostCategory> PostCategories { get; set; }
+        public ICollection<PostTag> PostTags { get; set; }
+        public ICollection<PostVideo> PostVideos { get; set; }
+        public ICollection<PostLike> PostLikes { get; set; }
     }
 }
