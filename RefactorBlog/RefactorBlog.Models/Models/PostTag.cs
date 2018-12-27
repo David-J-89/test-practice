c24c4d2682b1ede8916c6ca9e0ba5c1b269945f7
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:54:42 -0600

    Model for PostTag

--- RefactorBlog/RefactorBlog.Models/Models/PostTag.cs
+++ RefactorBlog/RefactorBlog.Models/Models/PostTag.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RefactorBlog.Models.Models
 {
     class PostTag
     {
+        [Key]
+        [Column(Order = 0)]
+        public string PostId { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public string TagId { get; set; }
+
+        public bool Checked { get; set; }
+
+        public Post Post { get; set; }
+
+        public Tag Tag { get; set; }
     }
 }
