bc1315712b5def908cf50c407eaf6eb7dacb85de
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 13:52:35 -0600

    Model for Reply

--- RefactorBlog/RefactorBlog.Models/Models/Reply.cs
+++ RefactorBlog/RefactorBlog.Models/Models/Reply.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RefactorBlog.Models.Models
 {
     class Reply
     {
+        public string Id { get; set; }
+
+        public string PostId { get; set; }
+
+        public string CommentId { get; set; }
+
+        public string ParentReplyId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Username { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [DefaultValue(false)]
+        public bool Deleted { get; set; }
+
+        public Post Post { get; set; }
+
+        public Comment Comment { get; set; }
+
+        public ICollection<ReplyLike> ReplyLikes { get; set; }
     }
 }
