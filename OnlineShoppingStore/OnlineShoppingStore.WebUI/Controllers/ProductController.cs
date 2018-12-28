762297e9d5d18351d7f36a12438157e9a1804a27
David Whaley <davidwhaleycode@outlook.com>
2018-12-28 12:10:57 -0600

    productcontroller constructor

--- OnlineShoppingStore/OnlineShoppingStore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingStore/OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -1,12 +1,19 @@
-﻿using System.Web.Mvc;
+﻿using OnlineShoppingStore.Domain.Abstract;
+using System.Web.Mvc;
 
 namespace OnlineShoppingStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IProductRepository repository;
+
+        public ProductController(IProductRepository repo) //generate constructor first, then autogen the readonly field.
+        {
+            repository = repo;
+        }
         public ViewResult List()
         {
-            return View();
+            return View(repository.Products);
         }
     }
 }
