0bc1a0c5a4b8c004ce0571bd844d9d73a74f71e8
David Whaley <davidwhaleycode@outlook.com>
2018-12-28 12:07:21 -0600

    ninject- bind mocking object to the repository

--- OnlineShoppingStore/OnlineShoppingStore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingStore/OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace OnlineShoppingStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
-        public ActionResult Index()
+        public ViewResult List()
         {
             return View();
         }
