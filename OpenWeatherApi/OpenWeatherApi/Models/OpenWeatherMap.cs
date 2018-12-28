505b616dd8d64be096b18700aaf9d279ad57f498
David Whaley <davidwhaleycode@outlook.com>
2018-12-27 23:08:42 -0600

    api model

---
+++ OpenWeatherApi/OpenWeatherApi/Models/OpenWeatherMap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace OpenWeatherApi.Models
+{
+    public class OpenWeatherMap
+    {
+        public string apiResponse { get; set; }
+
+        public Dictionary<string, string> cities
+        {
+            get; set;
+        }
+    }
+}
