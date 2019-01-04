411d5032b4013795bde4968c18dc8dce7d0cba70
David Whaley <davidwhaleycode@outlook.com>
2019-01-03 18:39:42 -0600

    InitofAngularAPITessting

---
+++ DatingApp.API/DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext //DbContext represents a session with the database, it's what allows us to query and save instances of our entities. ":" allows us to inherit from that class.
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
