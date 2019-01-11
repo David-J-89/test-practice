18abd2d5fd892c9d1d6e866c602dddfb3ae60ded
david-j-89 <davidwhaleycode@outlook.com>
2019-01-11 12:24:55 -0600

    models for db

---
+++ LibraryAPI/LibraryAPI/Models/CheckoutHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models
+{
+    public class CheckoutHistory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public LibraryAsset LibraryAsset { get; set; }
+
+        [Required]
+        public LibraryCard LibraryCard { get; set; }
+
+        [Required]
+        public DateTime CheckedOut { get; set; }
+
+        public DateTime? CheckedIn { get; set; }
+
+    }
+}
---
+++ LibraryAPI/LibraryAPI/Models/Video.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models
+{
+    public class Video : LibraryAsset
+    {
+        [Required]
+        public string Director { get; set; }
+    }
+}
