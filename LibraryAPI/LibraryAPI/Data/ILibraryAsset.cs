6342d96ff392d95a4032ae841a6b67d5ee2764b2
david-j-89 <davidwhaleycode@outlook.com>
2019-01-11 12:27:31 -0600

    LibraryContext

---
+++ LibraryAPI/LibraryAPI/Data/ILibraryAsset.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Data
+{
+    public interface ILibraryAsset
+    {
+        IEnumerable<LibraryAsset> GetAll(); //return all objects of this type
+        LibraryAsset GetById(int id); //get instance of objects by id prop
+
+        void Add(LibraryAsset newAsset); //ability to add new objects of this type to the dataset.
+        string GetAuthorOrDirector(int id);
+        string GetDeweyIndex(int id);
+        string GetType(int id);
+        string GetTitle(int id);
+        string GetIsbn(int id);
+
+        LibraryBranch GetCurrentLocation(int id);
+    }
+}
--- LibraryAPI/LibraryAPI/Data/LibraryContext.cs
+++ LibraryAPI/LibraryAPI/Data/LibraryContext.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibraryAPI.Data
 {
     public class LibraryContext : DbContext
     {
+        public LibraryContext(DbContextOptions options) : base(options) { } //constructor to pass the context options to.
 
+        public DbSet<Book> Books { get; set; } //dbsets create tables in the db for each of the entity models
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Checkout> Checkouts { get; set; }
+        public DbSet<CheckoutHistory> CheckoutHistories { get; set; }
+        public DbSet<LibraryBranch> LibraryBranches { get; set; }
+        public DbSet<BranchHours> BranchHours { get; set; }
+        public DbSet<LibraryCard> LibraryCards { get; set; }
+        public DbSet<Patron> Patrons { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<LibraryAsset> LibraryAssets { get; set; }
+        public DbSet<Hold> Holds { get; set; }
     }
 }
